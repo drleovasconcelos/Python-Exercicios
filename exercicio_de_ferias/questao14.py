46bb11cb6dcb94586c735390b3c4b9e6961d8198
drleovasconcelos <lfreire1985@gmail.com>
2023-12-22 14:17:42 -0300

    quinta commit

---
+++ exercicio_de_ferias/questao14.py
@@ -0,0 +1,14 @@
+# Faça um programa que exiba todos os números pares de 1 a 100 com o FOR e a função
+# RANGE. Após, faça o mesmo exercício utilizando o WHILE.
+
+num = 1
+while num <= 100:
+    if num % 2 == 0:
+        print(num)
+    num += 1
+        
+
+for i in range (1, 101):
+    if i % 2 == 0:
+        print(i)
+   
