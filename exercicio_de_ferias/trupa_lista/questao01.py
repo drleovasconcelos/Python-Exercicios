0890c1d5fb6f6165c6690a83621985a02e0223e5
drleovasconcelos <lfreire1985@gmail.com>
2024-01-09 08:03:38 -0300

    nono commit

---
+++ exercicio_de_ferias/trupa_lista/questao01.py
@@ -0,0 +1,15 @@
+# Crie um arquivo Python exercicio01.py. Após, defina uma variável do tipo lista (list)
+# vazia. Na sequência, implemente um programa para perguntar ao usuário se ele deseja
+# adicionar itens à lista de compras. Se sim, o usuário deve adicionar itens à lista
+# enquanto (while) não for digitada a palavra "FIM". Ao final, a lista de compras com os
+# itens adicionados pelo usuário. Dica: utilize a função .append().
+
+lista = []
+compras = int(input("Deseja cadastrar um item? 1 - Sim ou 2 - Não = "))
+while compras == 1:
+    item = input("Adicionar itens: ")
+    lista.append(item)
+    print("A Lista é: ", lista)
+    compras = int(input("Deseja cadastrar um item? 1 - Sim ou 2 - Não = "))
+else:
+    terminar = input("Fim")
---
+++ exercicio_de_ferias/trupa_lista/questao03.py
@@ -0,0 +1,7 @@
+# A partir do arquivo criado na atividade anterior, crie um arquivo Python exercicio03.py.
+# Após, implemente o mesmo algoritimo do exercício anteriore, e utilize as funções
+# .index() e .pop() para resolver o problema. Além disso, implemente uma nova variável
+# do tipo lista (list) e adicione a ela todos os elementos removidos. Ao final, além de exibir
+# a lista de compras atualizada, exiba a lista dos itens removidos. Dica: primeiro resolva
+# o problema com as funções indicadas; após, implemente a lista de itens removidos.
+
