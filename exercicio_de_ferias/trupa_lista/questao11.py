cf3085c6c155a780867f496ab800d57a3df4c176
drleovasconcelos <lfreire1985@gmail.com>
2024-03-28 10:11:56 -0300

    16commit

--- exercicio_de_ferias/trupa_lista/questao11.py
+++ exercicio_de_ferias/trupa_lista/questao11.py
@@ -4,3 +4,24 @@
 # computador os votos de cada candidato. Assim, o índice 0 da lista 'lista_votos' deve conter os
 # votos do índice 0 da 'lista_candidatos'. Finalmente, exiba na tela (terminal) a mensagem
 # conforme o exemplo abaixo:
+
+# 01 - defina duas variável do tipo lista (list): 'lista_candidatos' e 'lista_votos'. 
+# 02 - Na primeira, adicione manualmente os seguintes candidatos: Tião do Gás, Zezinho 71 e Maria Marimar. 
+# 03 - Na segunda, reserve com 0 os espaços para computador os votos de cada candidato. 
+# Assim, o índice 0 da lista 'lista_votos' deve conter os votos do índice 0 da 'lista_candidatos'. 
+#Finalmente, exiba na tela (terminal) a mensagem
+
+
+lista_candidatos = ["Tião do Gás", "Zezinho 71", "Maria Marimar", "Nulo", "Branco"]
+lista_votos = [52, 71, 98, 0, 1]
+j = 0
+i = 0
+d = {}
+for j, i in lista_candidatos, lista_votos:
+    
+
+    print("[", candidato , "]", votos )
+
+
+print("ELEIÇÕES MUNICIPAIS 2067")
+print("Cargo do Pleito: Vereador Munipal")
--- exercicio_de_ferias/trupa_lista/rascunho.py
+++ exercicio_de_ferias/trupa_lista/rascunho.py
@@ -1,10 +1,21 @@
-lista = []
-# Usando o Input() vamos coletar do usuário qual o número a ser adicionado.
-for i in range(15):
-    numero = int(input('Informe o número: '))
-    lista.append(numero)
-    
-for i,l in enumerate(lista):
-    
-    print(i+1, " -> ", l)
-    
+valores = []
+for i in range(10):
+    valor = int(input(f"Digite o {i+1}º valor: "))
+    valores.append(valor)
+
+numeros_pares = []
+numeros_impares = []
+for valor in valores:
+    if valor % 2 == 0:
+        numeros_pares.append(valor)
+    else:
+        numeros_impares.append(valor)
+
+print("Números pares:", numeros_pares)
+print("Números ímpares:", tuple(numeros_impares))
+print("Quantidade de números pares:", len(numeros_pares))
+print("Quantidade de números ímpares:", len(numeros_impares))
+soma_pares = sum(numeros_pares)
+soma_impares = sum(numeros_impares)
+print("Soma dos números pares:", soma_pares)
+print("Soma dos números ímpares:", soma_impares)
